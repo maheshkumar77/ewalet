9cba0c1ec03791bb9a27b4cfb8e7c071e124f795
maheshkumar77 <149643073+maheshkumar77@users.noreply.github.com>
2025-05-01 22:29:11 +0530

    Add files via upload

---
+++ Text5.js
@@ -0,0 +1,20 @@
+
+
+public class Test5 {
+
+	int i;
+	void display() {
+		
+		System.out.println(i);
+	}
+		
+
+	}
+	class Test3{
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Test5 b=new Test5();
+			b.i=10;
+			b.display();
+		}
+	}
